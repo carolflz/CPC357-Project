a2dd2217ad14e6acdca34ee7ff10e86b77cf72f8
Foo Ling Zhen <92304638+carolflz@users.noreply.github.com>
2025-01-14 13:29:17 +0800

    Add files via upload
    Upload vonesetting.h with initial settings for VONE system

---
+++ vonesetting.h
@@ -0,0 +1,17 @@
+#if !defined (VONESETTING_H)
+#define VONESETTING_H
+#else
+#error Multiple includes of vonesetting.h
+#endif
+
+#define WIFI_SSID "WIFI"          //Replace this with YOUR WiFi SSID
+#define WIFI_PASSWORD "PASSWORD"  //Replace this with YOUR WiFi Password
+
+#define MQTT_SERVER "mqtt.v-one.my"
+#define MQTT_PORT 8883
+#define MQTT_USERNAME "49iLSRJWRz2bNXqf"  //Replace this with the Access Token of YOUR gateway
+#define MQTT_PASSWORD " "
+
+#define GATEWAYID "987459be-df32-4452-80bb-b74ea47b20b6"  //Replace this with the gatewayID of your gateway
+#define INTERVAL 900000 //15 min
+#define INTERVAL2 500 //0.5S
